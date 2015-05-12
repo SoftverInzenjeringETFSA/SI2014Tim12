74d66ad4169e04a996642866335a0879e02a49c4
SumejjaPorca <sumejja93@gmail.com>
2015-05-13 01:01:36 +0200

    Kreiran Maven projekat

---
+++ Projekt/zubarska-ordinacija/src/main/java/ba/unsa/etf/si/tim12/App.java
@@ -0,0 +1,13 @@
+package ba.unsa.etf.si.tim12;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
