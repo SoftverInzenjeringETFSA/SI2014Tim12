6d2ce10c4d2275eedf7daad177bc0b5f9fe036a3
selmatucak <stucak1@etf.unsa.ba>
2015-05-16 00:16:30 +0200

    View Modeli 2

--- Projekt/zubarska-ordinacija/src/main/java/ba/unsa/etf/si/tim12/bll/viewmodel/NoviOZahvatMaterijalVM.java
+++ Projekt/zubarska-ordinacija/src/main/java/ba/unsa/etf/si/tim12/bll/viewmodel/NoviOZahvatMaterijalVM.java
@@ -1,18 +1,22 @@
 package ba.unsa.etf.si.tim12.bll.viewmodel;
 
 public class NoviOZahvatMaterijalVM {
-	private long materijalId;
-	private double kolicina;
-	public long getMaterijalId() {
-		return materijalId;
-	}
-	public void setMaterijalId(long materijalId) {
-		this.materijalId = materijalId;
-	}
-	public double getKolicina() {
-		return kolicina;
-	}
-	public void setKolicina(double kolicina) {
-		this.kolicina = kolicina;
-	}
+private long materijalId;
+private double kolicina; 
+
+public long getMaterijalId() {
+	return materijalId;
+}
+
+public void setMaterijalId (long materijalId) {
+	this.materijalId=materijalId;
+}
+
+public double getKolicina() {
+	return kolicina;
+}
+
+public void setKolicina (double kolicina) {
+	this.kolicina=kolicina;
+}
 }
