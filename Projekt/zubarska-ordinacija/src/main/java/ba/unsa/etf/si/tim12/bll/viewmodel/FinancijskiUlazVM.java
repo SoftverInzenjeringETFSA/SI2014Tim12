c0fc9967c0475b56c01c0cfe33023b5d6dada668
selmatucak <stucak1@etf.unsa.ba>
2015-05-16 00:33:39 +0200

    Ispravke Review Modela

--- Projekt/zubarska-ordinacija/src/main/java/ba/unsa/etf/si/tim12/bll/viewmodel/FinancijskiUlazVM.java
+++ Projekt/zubarska-ordinacija/src/main/java/ba/unsa/etf/si/tim12/bll/viewmodel/FinancijskiUlazVM.java
@@ -1,5 +1,49 @@
 package ba.unsa.etf.si.tim12.bll.viewmodel;
 
-public class FinancijskiUlazVM {
+import java.util.Date;
+import java.util.ArrayList;
 
+
+public class FinancijskiUlazVM {
+	private Date vrijemeOd;
+	private Date vrijemeDo;
+	private double ukupnaCijena;
+	private int ukupnoPosjeta;
+	private ArrayList<FinancijskiUlazRowVM> listaFinancijskiUlazRowVM;
+
+	public FinancijskiUlazVM() {
+		listaFinancijskiUlazRowVM=new ArrayList<FinancijskiUlazRowVM>();
+	}
+	public Date getVrijemeOd() {
+		return vrijemeOd;
+	}
+	public void setVrijemeOd (Date vrijemeOd) {
+		this.vrijemeOd=vrijemeOd;
+	}
+
+	public Date getVrijemeDo() {
+		return vrijemeDo;
+	}
+	public void setVrijemeDo (Date vrijemeDo) {
+		this.vrijemeDo=vrijemeDo;
+	}
+	public double getUkupnaCijena() {
+		return ukupnaCijena;
+	}
+	public void setUkupnaCijena(double ukupnaCijena) {
+		this.ukupnaCijena = ukupnaCijena;
+	}
+	public int getUkupnoPosjeta() {
+		return ukupnoPosjeta;
+	}
+	public void setUkupnoPosjeta(int ukupnoPosjeta) {
+		this.ukupnoPosjeta = ukupnoPosjeta;
+	}
+
+	public ArrayList<FinancijskiUlazRowVM> geListaFinancijskiUlazRowVM() {
+		return listaFinancijskiUlazRowVM;
+	}
+	public void setFinancijskiUlazRowVM(ArrayList<FinancijskiUlazRowVM> listaFinancijskiUlazRowVM) {
+		this.listaFinancijskiUlazRowVM = listaFinancijskiUlazRowVM;
+	}
 }
