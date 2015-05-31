fa76c304385c3d55e5f43b1d35924314cbb4a9f3
luka454 <pejovicluka454@gmail.com>
2015-05-31 17:39:57 +0200

    Validator imena prepoznaje naša slova

--- Projekt/zubarska-ordinacija/src/main/java/ba/unsa/etf/si/tim12/ui/components/Validator.java
+++ Projekt/zubarska-ordinacija/src/main/java/ba/unsa/etf/si/tim12/ui/components/Validator.java
@@ -39,7 +39,7 @@ public class Validator {
 	public static void ValidateName(String text) throws Exception{
 		if(text.length() < 3)
 			throw new Exception("Ime mora imati najmanje 3 karaktera");
-		if(!text.matches("^[a-zA-Z \\.]*$"))
+		if(!text.matches("^[a-zA-Z \\.ćĆčČšŠđŠžŽ]*$"))
 			throw new Exception("Ime može imati samo slova, razmake i tačke.");
 		if(text.matches("^.*[ ]{2,}.*$"))
 			throw new Exception("Ime ne može imati duple razmake.");
