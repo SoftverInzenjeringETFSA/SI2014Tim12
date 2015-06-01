cd6cb6c18d39dfb34492c629226f134795ce51e4
Emina Šarić <esaric1@etf.unsa.ba>
2015-06-01 14:45:04 +0200

    IzvjestajPosjeteDSM

--- Projekt/zubarska-ordinacija/src/main/java/ba/unsa/etf/si/tim12/ui/IzvjestajPosjeteDSM.java
+++ Projekt/zubarska-ordinacija/src/main/java/ba/unsa/etf/si/tim12/ui/IzvjestajPosjeteDSM.java
@@ -1,6 +1,7 @@
 package ba.unsa.etf.si.tim12.ui;
 import java.awt.EventQueue;
 
+import javax.swing.ButtonGroup;
 import javax.swing.JFrame;
 import javax.swing.JScrollPane;
 
@@ -37,11 +38,18 @@ import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.Date;
 
+import javax.swing.JRadioButton;
+
 
 public class IzvjestajPosjeteDSM {
 	private JDialog frame;
 	private JTable table;
 	private JSpinner spinnerOd;
+	private JSpinner spinnerDo;
+	private JRadioButton rdbtnDnevni;
+	private JRadioButton rdbtnSedmini;
+	private JRadioButton rdbtnMjeseni;
+	private Calendar calendar;
 	final static Logger logger = Logger.getLogger(IzvjestajPosjeteDSM.class);
 	
 	/**
@@ -74,16 +82,16 @@ public class IzvjestajPosjeteDSM {
 	private void initialize() {		
 		frame = new JDialog();
 		MainForma.Prekini(frame);
-		frame.setTitle("Izvještaj o posjetama po danu");
+		frame.setTitle("Izvještaj o posjetama");
 		frame.setModalityType(ModalityType.APPLICATION_MODAL);
 		frame.setResizable(false);
 		
-		frame.setBounds(100, 100, 550, 320);
+		frame.setBounds(100, 100, 550, 390);
 		frame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		frame.getContentPane().setLayout(null);
 		frame.setLocationRelativeTo(null);
 		JScrollPane scrollPane = new JScrollPane();
-		scrollPane.setBounds(22, 80, 503, 157);
+		scrollPane.setBounds(22, 147, 503, 157);
 		frame.getContentPane().add(scrollPane);
 		
 		table = new JTable();
@@ -100,12 +108,12 @@ public class IzvjestajPosjeteDSM {
 		table.setSelectionMode(javax.swing.ListSelectionModel.SINGLE_SELECTION);
 		scrollPane.setViewportView(table);
 		
-		JLabel lblPretraivanjePo = new JLabel("Odaberite dan:");
-		lblPretraivanjePo.setBounds(22, 13, 204, 19);
+		JLabel lblPretraivanjePo = new JLabel("Odaberite vremenski interval:");
+		lblPretraivanjePo.setBounds(22, 13, 176, 19);
 		frame.getContentPane().add(lblPretraivanjePo);
 		
 		JButton btnModifikacijaMaterijala = new JButton("Prikaži");
-		btnModifikacijaMaterijala.setBounds(263, 250, 121, 23);
+		btnModifikacijaMaterijala.setBounds(263, 317, 121, 23);
 		btnModifikacijaMaterijala.addActionListener(new ActionListener() {
 			
 			public void actionPerformed(ActionEvent e) {
@@ -113,8 +121,18 @@ public class IzvjestajPosjeteDSM {
 				try {
 					sesija = HibernateUtil.getSessionFactory().openSession();
 					IzvjestajManager izvjestaji = new IzvjestajManager(sesija);
-					OdradjenePosjeteVM posjete = izvjestaji.odradjenePosjetePoDanu((Date) spinnerOd.getValue());
-					ArrayList<OdradjenePosjeteRowVM> redovi = posjete.getOdradjenePosjete();					
+					ArrayList<OdradjenePosjeteRowVM> redovi = null;
+					
+					if(rdbtnDnevni.isSelected()) {
+						OdradjenePosjeteVM posjete = izvjestaji.odradjenePosjetePoDanu((Date) spinnerOd.getValue());
+						redovi = posjete.getOdradjenePosjete();					
+					}
+					
+					if(rdbtnSedmini.isSelected() || rdbtnMjeseni.isSelected()) {
+						OdradjenePosjeteVM posjete = izvjestaji.odradjenePosjete((Date) spinnerOd.getValue(), (Date) spinnerDo.getValue()); 
+						redovi = posjete.getOdradjenePosjete();
+					}
+					
 					Object[][] data = new Object[redovi.size()][];
 					
 					for(int i = 0; i < redovi.size(); i++){
@@ -140,7 +158,7 @@ public class IzvjestajPosjeteDSM {
 		frame.getContentPane().add(btnModifikacijaMaterijala);
 		
 		JButton btnOdustani = new JButton("Odustani");
-		btnOdustani.setBounds(404, 250, 121, 23);
+		btnOdustani.setBounds(404, 317, 121, 23);
 		btnOdustani.addActionListener(new ActionListener() {
 			
 			
@@ -152,12 +170,67 @@ public class IzvjestajPosjeteDSM {
 		});
 		frame.getContentPane().add(btnOdustani);
 		
-		Calendar calendar = Calendar.getInstance();
+		calendar = Calendar.getInstance();
 		Date initDate = calendar.getTime();
 		spinnerOd = new JSpinner();
 		spinnerOd.setModel(new SpinnerDateModel(new Date(-2208992399907L), null, null, Calendar.DAY_OF_YEAR));
 		spinnerOd.setEditor(new JSpinner.DateEditor(spinnerOd, "dd/MM/yyyy"));
-		spinnerOd.setBounds(22, 45, 171, 22);
+		spinnerOd.setBounds(227, 47, 171, 22);
 		frame.getContentPane().add(spinnerOd);
+		
+		spinnerDo = new JSpinner();
+		spinnerDo.setBounds(225, 91, 171, 22);
+		spinnerDo.setModel(new SpinnerDateModel(initDate, null, null, Calendar.DAY_OF_YEAR));
+		spinnerDo.setEditor(new JSpinner.DateEditor(spinnerDo, "dd/MM/yyyy"));
+		frame.getContentPane().add(spinnerDo);
+		
+		JLabel lblDo = new JLabel("Do:");
+		lblDo.setBounds(194, 94, 28, 16);
+		frame.getContentPane().add(lblDo);
+		
+		JLabel lblOd = new JLabel("Od:");
+		lblOd.setBounds(194, 50, 28, 16);
+		frame.getContentPane().add(lblOd);
+		
+		rdbtnDnevni = new JRadioButton("Dnevni");
+		rdbtnDnevni.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent arg0) {
+				spinnerDo.setValue(spinnerOd.getValue());
+			}
+		});
+		rdbtnDnevni.setSelected(true);
+		rdbtnDnevni.setBounds(22, 41, 72, 25);
+		frame.getContentPane().add(rdbtnDnevni);
+		
+		rdbtnSedmini = new JRadioButton("Sedmični");
+		rdbtnSedmini.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Date datumDo = (Date) spinnerOd.getValue();
+				calendar.setTime(datumDo);
+				calendar.set(Calendar.DATE, calendar.get(Calendar.DATE) + 6);
+				datumDo = calendar.getTime();
+				spinnerDo.setValue(datumDo);
+			}
+		});
+		rdbtnSedmini.setBounds(22, 71, 81, 25);
+		frame.getContentPane().add(rdbtnSedmini);
+		
+		rdbtnMjeseni = new JRadioButton("Mjesečni");
+		rdbtnMjeseni.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				Date datumDo = (Date) spinnerOd.getValue();
+				calendar.setTime(datumDo);
+				calendar.set(Calendar.MONTH, calendar.get(Calendar.MONTH) + 1);
+				datumDo = calendar.getTime();
+				spinnerDo.setValue(datumDo);
+			}
+		});
+		rdbtnMjeseni.setBounds(22, 101, 88, 25);
+		frame.getContentPane().add(rdbtnMjeseni);
+		
+		ButtonGroup rbuttons = new ButtonGroup();
+		rbuttons.add(rdbtnMjeseni);
+		rbuttons.add(rdbtnSedmini);
+		rbuttons.add(rdbtnDnevni);
 	}
 }
--- Radni materijali/init.sql
+++ Radni materijali/init.sql
@@ -11,7 +11,7 @@ SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 -- -----------------------------------------------------
 -- Schema SI2014Tim12
 -- -----------------------------------------------------
-CREATE SCHEMA IF NOT EXISTS `SI2014Tim12` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+CREATE SCHEMA IF NOT EXISTS `SI2014Tim12` DEFAULT CHARACTER SET utf8 COLLATE utf8_slovenian_ci ;
 USE `SI2014Tim12` ;
 
 -- -----------------------------------------------------
